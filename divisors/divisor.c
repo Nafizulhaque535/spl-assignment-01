fe014207c7264936ee6f0f88555236496a8bfc2a
Nafizulhaque535 <codeglobal35@gmail.com>
2020-08-18 06:38:22 +0600

    divisor program added

---
+++ divisors/divisor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int x, i;
+	printf("Enter a number: ");
+	scanf("%d", &x);
+	printf("All the divisor of %d are: ", x);
+
+	// condition of a divisor value
+	for(i = 1; i <= x; i++) {
+		if((x%i) == 0){
+			printf("%d", i);
+			printf("\t");   // printing tab
+		}
+	}
+	return 0;
+}
